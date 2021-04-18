4e2e70597d326d2892294032462f941c158557e3
Botean_Catalin-Cristian <catalin.botean@softstatetechnologies.com>
2021-04-18 16:52:30 +0300

    Added Polyline Component

--- pages/map/MapPage.tsx
+++ pages/map/MapPage.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Text, View } from "react-native";
-import MapView, { AnimatedRegion, Marker } from "react-native-maps";
+import MapView, { Polyline, Marker } from "react-native-maps";
 
 import { styles } from "./MapPage.style";
 
@@ -9,14 +9,25 @@ export function MapPage() {
   const [region, setRegion] = useState({
     latitude: 45.695908,
     longitude: 21.894043,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitudeDelta: 0.0022,
+    longitudeDelta: 0.0021,
   });
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Aplicatie Nevazatori</Text>
       <MapView style={styles.map} region={region} onRegionChange={setRegion}>
+        <Polyline
+          coordinates={[
+            { latitude: 45.695632, longitude: 21.893906 },
+            { latitude: 45.695719, longitude: 21.894042 },
+            { latitude: 45.695774, longitude: 21.894148 },
+            { latitude: 45.695842, longitude: 21.894071 },
+            { latitude: 45.695908, longitude: 21.894043 },
+          ]}
+          strokeWidth={3}
+          strokeColor="#f00"
+        />
         <Marker
           ref={(marker) => {
             marker = marker;
