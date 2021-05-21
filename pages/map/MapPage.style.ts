7d2eaab7fb405188564c3f009b8f55f115cc6d9d
Botean_Catalin-Cristian <catalin.botean@softstatetechnologies.com>
2021-05-21 19:27:43 +0300

    Changes regarding style on MapPage

--- pages/map/MapPage.style.ts
+++ pages/map/MapPage.style.ts
@@ -3,28 +3,333 @@ import { Dimensions, StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F6C460",
+    backgroundColor: "#342D73",
     paddingTop: "15%",
     alignItems: "center",
     justifyContent: "flex-start",
   },
   text: {
-    fontSize: 20,
-    color: "#000",
+    fontSize: 23,
+    color: "#ffffff",
+    alignSelf: "center",
+    paddingLeft: 10,
+    fontWeight: "700",
   },
   map: {
-    marginTop: "5%",
+    alignSelf: "center",
+    marginTop: 15,
     height: 250,
     width: "100%",
+    marginBottom: 15,
   },
   scrollContent: {
     width: Dimensions.get("window").width,
   },
   item: {
-    padding: 20,
     width: "100%",
     marginTop: 12,
-    fontSize: 27,
+    fontSize: 25,
     borderRadius: 4,
+    color: "#fff",
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  lowerView: {
+    backgroundColor: "#fff",
+    height: "70%",
+    width: "100%",
+    marginTop: 70,
+    borderTopColor: "#000000",
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+  },
+  logo: {
+    height: 40,
+    width: 40,
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "baseline",
+  },
+  orangeText: {
+    color: "#FFA500",
+  },
+
+  orangeView: {
+    display: "flex",
+    marginTop: 30,
+    backgroundColor: "#FFA500",
+    height: 280,
+    width: "100%",
+  },
+
+  boxView: {
+    display: "flex",
+    alignItems: "center",
+    height: 60,
+    width: 220,
+    backgroundColor: "#342D73",
+    marginTop: 20,
+    marginLeft: 20,
+    borderRadius: 40,
+    shadowColor: "#000",
+    shadowOffset: { width: 100, height: 100 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+  },
+  infoView: {
+    display: "flex",
+    height: 100,
+    width: 250,
+    backgroundColor: "#023E58",
+    marginTop: 40,
+    marginLeft: 300,
+    marginRight: 30,
+    borderRadius: 40,
+  },
+  infoText: {
+    color: "#fff",
+    alignSelf: "flex-start",
+    fontSize: 17,
+    paddingLeft: 40,
+    paddingTop: 7,
   },
 });
+
+export const MapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#8ec3b9",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1a3646",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4b6878",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#64779e",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4b6878",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#334e87",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#283d6a",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6f9ba5",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#3C7680",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#304a7d",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#98a5be",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2c6675",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#255763",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#b0d5ce",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#98a5be",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "transit.line",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#283d6a",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#3a4762",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#0e1626",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#4e6d70",
+      },
+    ],
+  },
+];
--- pages/map/MapPage.tsx
+++ pages/map/MapPage.tsx
@@ -1,29 +1,30 @@
-import {StatusBar} from "expo-status-bar";
-import React, {useEffect, useState} from "react";
-import {ScrollView, Text, TouchableOpacity, View} from "react-native";
-import MapView, {AnimatedRegion, Marker, Polyline} from "react-native-maps";
-import {useDispatch, useSelector} from "react-redux";
-import {ApplicationState} from "../../store";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Text, TouchableOpacity, View, Image } from "react-native";
+import MapView, { AnimatedRegion, Marker, Polyline } from "react-native-maps";
+import { useDispatch, useSelector } from "react-redux";
+import { ApplicationState } from "../../store";
 import {
   getData,
   refetchData,
   selectDaysWithWalks,
   selectLastGPSData,
 } from "../../store/data";
-import {styles} from "./MapPage.style";
+
+import { styles, MapStyle } from "./MapPage.style";
 
 export function MapPage() {
-  const [region, setRegion] = useState({
-    latitude: 45.695908,
-    longitude: 21.894043,
-    latitudeDelta: 0.0022,
-    longitudeDelta: 0.0021,
-  });
-
-  const dispatch = useDispatch();
   const dataInfo = useSelector((state: ApplicationState) =>
     selectLastGPSData(state.GPS.dataGPS)
   );
+  const [region, setRegion] = useState({
+    latitude: dataInfo.location.latitude,
+    longitude: dataInfo.location.longitude,
+    latitudeDelta: 0.006,
+    longitudeDelta: 0.006,
+  });
+
+  const dispatch = useDispatch();
 
   const informatii = useSelector((state: ApplicationState) =>
     selectDaysWithWalks(state.GPS.dataGPS)
@@ -44,21 +45,44 @@ export function MapPage() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Aplicatie Nevazatori</Text>
-      <MapView style={styles.map} region={region} onRegionChange={setRegion}>
-        <Marker
-          ref={(marker) => {
-            marker = marker;
-          }}
-          coordinate={region}
-        />
-      </MapView>
+      <View style={styles.header}>
+        <Image source={require("./logo.png")} style={styles.logo} />
+        <Text style={styles.text}>
+          <Text style={styles.orangeText}>BLIND</Text>Helper
+        </Text>
+      </View>
+      <View style={styles.orangeView}>
+        <MapView
+          style={styles.map}
+          region={region}
+          onRegionChange={setRegion}
+          customMapStyle={MapStyle}
+          zoomEnabled
+          mapType="mutedStandard"
+        >
+          <Marker coordinate={region} />
+        </MapView>
+      </View>
       {/* <ScrollView contentContainerStyle={styles.scrollContent}> */}
       {/* {dataInfo.map((value, index) => ( */}
-      <Text style={styles.item}>
-        {dataInfo.location.latitude},{dataInfo.location.longitude},{" "}
-        {dataInfo.hour}
-      </Text>
+      <View style={styles.lowerView}>
+        <View style={styles.boxView}>
+          <Text style={styles.item}>
+            Last seen on:
+            {/* {dataInfo.location.latitude},{dataInfo.location.longitude},{" "}
+            {dataInfo.hour} */}
+          </Text>
+          <View style={styles.infoView}>
+            <Text style={styles.infoText}>
+              Latitude: {dataInfo.location.latitude}
+            </Text>
+            <Text style={styles.infoText}>
+              Longitude: {dataInfo.location.longitude}
+            </Text>
+            <Text style={styles.infoText}>Hour: {dataInfo.hour}</Text>
+          </View>
+        </View>
+      </View>
       {/* ))} */}
       {/* </ScrollView> */}
       <StatusBar style="auto" />
